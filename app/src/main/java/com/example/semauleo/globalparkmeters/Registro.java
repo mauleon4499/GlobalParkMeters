a2b2eab192599a73519745f7a76e3ba1637b3d81
Sergio Mauleón Cristóbal <semauleo@unirioja.loc>
2017-02-23 17:49:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/semauleo/globalparkmeters/Registro.java
@@ -0,0 +1,17 @@
+package com.example.semauleo.globalparkmeters;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by semauleo on 23/02/2017.
+ */
+
+public class Registro extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+    }
+}
